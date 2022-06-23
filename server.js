c0914802d3a5149d389837f056c3833be33c3407
linds0202 <lindsayarainey@gmail.com>
2022-06-23 11:14:13 -0700

    add mongodb connection & set up variable

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const cors = require ('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'shrinkflation-list-api',
+    collection = 
+
+MongoClient.connect(dbConnectionString)
+.then(client => {
+    console.log(`Connected to ${dbName} database`)
+    db = client.db(dbName)
+    collection = db.collection('items-list')
+})
