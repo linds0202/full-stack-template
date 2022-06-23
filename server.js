713fad42782dfacee8502f68d4c8e6ee2c186277
linds0202 <lindsayarainey@gmail.com>
2022-06-23 11:46:50 -0700

    added middleware

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ MongoClient.connect(dbConnectionString)
     collection = db.collection('items-list')
 })
 
+//middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`)
