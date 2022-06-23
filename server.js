3512b9a6af3a794eec6d29e12651c5b03e681260
linds0202 <lindsayarainey@gmail.com>
2022-06-23 11:38:11 -0700

    added port and mongodb connections

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const app = express()
 const cors = require ('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'shrinkflation-list-api',
-    collection = 
+    collection 
 
 MongoClient.connect(dbConnectionString)
 .then(client => {
     console.log(`Connected to ${dbName} database`)
     db = client.db(dbName)
     collection = db.collection('items-list')
+})
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
