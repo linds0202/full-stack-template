a9dc72b5cc77306a53a7db0a64336d4a77ca781a
linds0202 <lindsayarainey@gmail.com>
2022-06-23 12:25:40 -0700

    added additional CRUD

--- server.js
+++ server.js
@@ -24,12 +24,66 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
-app.get('/', async (req, res) => {
-    try {
-        res.render('index.ejs')
-    } catch (error) {
-        res.status(500).send({message: error.message})
-    }
+// app.get('/', async (req, res) => {
+//     try {
+//         res.render('index.ejs')
+//     } catch (error) {
+//         res.status(500).send({message: error.message})
+//     }
+// })
+
+//CRUD methods
+app.get('/', (req, res) => {
+    db.collection('items-list').find().toArray()
+        .then(data => {
+            let nameList = data.map(item => item.itemName)
+            console.log(nameList)
+            res.render('index.ejs', {info: nameList})
+        })
+        .catch(error => console.log(error))
+})
+
+app.post('/api', (req, res) => {
+    console.log('Post heard')
+    db.collection('items-list').insertOne(
+        req.body
+    )
+    .then(result => {
+        console.log(result)
+        res.redirect('/')
+    })
+})
+
+app.put('/updateEntry', (req, res) => {
+    console.log(req.body)
+    Object.keys(req.body).forEach(key => {
+        if (req.body[key] === null || req.body[key] === undefined || req.body[key] === '') {
+            delete req.body[key]
+        }
+    })
+    console.log(req.body)
+    db.collection('items-list').findOneAndUpdate(
+        {itemName: req.body.itemName},
+        {
+            $set: req.body
+        }
+    )
+    .then(result => {
+        console.log(result)
+        res.json('Success')
+    })
+    .catch(err => console.error(err))
+})
+
+app.delete('/deleteEntry', (req, res) => {
+    db.collection('items-list').deleteOne(
+        {itemName: req.body.itemName}
+    )
+    .then(result => {
+        console.log('Entry deleted')
+        res.json('Entry Deleted')
+    })
+    .catch(err => console.error(err))
 })
 
 
