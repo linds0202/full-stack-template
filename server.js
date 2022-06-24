9104ec21a05bcefe4fd2909e586bf76bab9412af
linds0202 <lindsayarainey@gmail.com>
2022-06-24 08:29:37 -0700

    updated index, server, env

--- server.js
+++ server.js
@@ -26,7 +26,13 @@ app.use(cors())
 
 app.get('/', async (req, res) => {
     try {
-         res.render('index.ejs')
+        db.collection('items-list').find().toArray()
+        .then(data => {
+            let nameList = data.map(item => item.itemName)
+            console.log(nameList)
+            res.render('index.ejs', {info: nameList})
+        })
+         //res.render('index.ejs')
     } catch (error) {
         res.status(500).send({message: error.message})
     }
